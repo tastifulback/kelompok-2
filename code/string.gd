3c83fb9081ac8959f7961470dd775212a0ab8f5b
tastifulback <tastifulback@gmail.com>
2026-05-12 15:51:29 +0700

    string not done

---
+++ code/string.gd
@@ -0,0 +1,25 @@
+extends Line2D
+@onready var ray = $RayCast2D
+@onready var hitmark = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var lastIndex  = get_point_count()-1
+	var mouse_pos = get_global_mouse_position()
+	set_point_position(lastIndex, mouse_pos)
+	ray.position = get_point_position(lastIndex)
+	ray.target_position = to_local(get_global_mouse_position()) - ray.position
+	if ray.is_colliding():
+		var target  = ray.get_collider()
+		
+		if target is test:
+			var hit = to_local(ray.get_collision_point())
+			if hitmark == false:
+				hitmark = true
+				add_point(hit)
+				print("added")
+	else : hitmark = false  
