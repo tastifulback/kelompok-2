1502fb8d411d7b23e158b8c6b30fb1ec7dcde0ac
tastifulback <tastifulback@gmail.com>
2026-05-15 03:32:32 +0700

    STRING SOMEWHAT DONE

--- code/player.gd
+++ code/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 class_name player
-
+@onready var STRING = preload("uid://dq7vh3iffl7ms")
+@onready var enemyList = PackedVector2Array([])
+@onready var enemyN = 0
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -15,6 +17,10 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 	if Input.is_action_just_pressed("s") and !is_on_floor():
 		velocity.y = -JUMP_VELOCITY
+	if Input.is_action_just_pressed("attack"):
+		stringSpawn()
+	if Input.is_action_just_released("attack"):
+		enemyKill()
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("a", "d")
@@ -22,5 +28,25 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
+
+func stringSpawn() ->void :
+	var Line : stringer = STRING.instantiate()
+	get_parent().add_child(Line)
+	Line.global_position = global_position
+	Line.enemies.connect(enemyListFunc)
+	Line.set_point_position(0, Vector2.ZERO) 
+	Line.set_point_position(1, Line.get_local_mouse_position())
+
+	
+func enemyListFunc(enemy):
+	enemyList.append(enemy)
+	print(enemyList)
+	
+func enemyKill():
+	for i in range(enemyList.size()):
+		global_position = enemyList[i]
+	enemyList.clear()
+	
+	
--- code/string.gd
+++ code/string.gd
@@ -1,7 +1,7 @@
 extends Line2D
-class_name string
+class_name stringer
 @onready var ray = $RayCast2D
-@onready var hitmark = false
+signal enemies(Vector2)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,16 +10,25 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	var lastIndex  = get_point_count()-1
-	var mouse_pos = get_global_mouse_position()
-	set_point_position(lastIndex, mouse_pos)
+	var mouse_pos = get_local_mouse_position()
+	ray.target_position = to_local(get_global_mouse_position()) - get_point_position(lastIndex)
 	ray.position = get_point_position(lastIndex)
-	ray.target_position = to_local(get_global_mouse_position()) - ray.position
+	
+	ray.force_raycast_update()
+	set_point_position(lastIndex, mouse_pos)
 	if ray.is_colliding():
 		var target  = ray.get_collider()
 		print(target)
 		if target is test:
-			var hit = to_local(ray.get_collision_point())
+			var hit_global = ray.get_collision_point()
+			var normal = ray.get_collision_normal() # The direction the wall is facing
+	
 			if !target.onHit():
-				
-				add_point(hit)
+				# Move the teleport target 10-20 pixels away from the surface
+				var safe_teleport_spot = hit_global + (normal) 
+		
+				enemies.emit(safe_teleport_spot)
+				add_point(to_local(hit_global))
 				print("added")
+	if Input.is_action_just_released("attack"):
+		queue_free()
