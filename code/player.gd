f5fd9275be9d748127c93e334bf25a29273c4b52
tastifulback <tastifulback@gmail.com>
2026-06-05 09:48:57 +0700

    jump fix

--- code/player.gd
+++ code/player.gd
@@ -30,7 +30,7 @@ func _physics_process(delta: float) -> void:
 			SPEED *= 3
 			dashTime.start()
 			dashAllow = false
-	if Input.is_action_just_pressed("w") or Input.is_action_just_pressed("space") and is_on_floor():
+	if Input.is_action_just_pressed("w")  and is_on_floor() or Input.is_action_just_pressed("space") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	if Input.is_action_just_pressed("s") and !is_on_floor():
 		velocity.y = -JUMP_VELOCITY
